3dc593ba268deb86defaa9d9e5084d43d7eb7e81
EdwinJOrtizCampos <edwinjavierortizcamposORCA1@HOTMAIL.COM>
2021-12-30 16:36:43 +0000

    Sexta versión de Kata6

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
-import branches.AmericanToyBusiness;
-import branches.AsianToyBusiness;
+import factories.regionalfactories.AmericanToyFactory;
+import factories.regionalfactories.AsianToyFactory;
 import java.util.Scanner;
-import business.ToyBusiness;
+import business.BusinessToy;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,8 +20,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ToyBusiness americanToyBusiness = new AmericanToyBusiness();
-        ToyBusiness asianToyBusiness = new AsianToyBusiness();
+        BusinessToy americanToyBusiness = new BusinessToy(new AmericanToyFactory());
+        BusinessToy asianToyBusiness = new BusinessToy(new AsianToyFactory());
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("Introducir comando: ");
@@ -33,16 +33,16 @@ public class Main {
                     System.exit(0);
                     break;
                 case "asiancar":
-                    asianToyBusiness.createToy("car");
+                    asianToyBusiness.produceToy("car");
                     break;
                 case "asianhelicopter":
-                    asianToyBusiness.createToy("helicopter");
+                    asianToyBusiness.produceToy("helicopter");
                     break;
                 case "americancar":
-                    americanToyBusiness.createToy("car");
+                    americanToyBusiness.produceToy("car");
                     break;
                 case "americanhelicopter":
-                    americanToyBusiness.createToy("helicopter");
+                    americanToyBusiness.produceToy("helicopter");
                     break;
                 default:
                     System.out.println("Command unknown!\n");
